f7c7f3159ba7fe651b3a957ff5aca6871331b083
sixaphone <miki.halilevic@gmail.com>
2020-11-17 14:36:37 +0100

    feat(mailing): fix template dir to ignore .env

--- src/config/directories.ts
+++ src/config/directories.ts
@@ -1,10 +1,17 @@
 import { IsDefined } from 'class-validator';
+import { Env } from '@meta5/nestjs-config';
+import { resolve } from 'path';
 
 export class Directories {
   @IsDefined()
   public readonly mailingTemplateDirectory: string;
 
   constructor() {
-    this.mailingTemplateDirectory = process.env.TEMPLATE_DIRECTORY;
+    this.mailingTemplateDirectory = resolve(
+      __dirname,
+      '../../',
+      process.env.NODE_ENV === Env.DEVELOPMENT ? 'src' : 'dist',
+      'templates'
+    );
   }
 }
